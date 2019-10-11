496e68d3f3c6ddf606ac40fb4214c1af26c8e35b
Dan <dan@createmarketing.co.uk>
2019-10-11 10:01:51 +0100

    added: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameStartComponent } from './game-start/game-start.component';
+import { GameMainComponent } from './game-main/game-main.component';
+import { GameResultsComponent } from './game-results/game-results.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GameStartComponent },
+  { path: 'game', component: GameMainComponent },
+  { path: 'results', component: GameResultsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
